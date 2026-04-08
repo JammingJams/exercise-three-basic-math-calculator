f1e747f27077529e75bcd43bfaf69cd439f3c5fa
JammingJams <noahlogan20o00@gmail.com>
2026-04-08 14:31:26 -0400

    Change string number -> letter

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,4 +1,42 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class MathApp {
+    public static void main(String[] args) {
+
+        int n1 = 5;
+        int n2 = 12;
+
+        String a = "A";
+        String s = "S";
+        String m = "M";
+        String d = "D";
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your letter: ");
+        String multname = keyboard.nextLine();
+
+        if (multname.equals("A")) {
+            System.out.println(n1 + n2);
+        }
+        else if (multname.equals("S")) {
+            System.out.println(n1 - n2);
+        }
+        else if (multname.equals("M")) {
+            System.out.println(n1 * n2);
+        }
+        else if (multname.equals("D")) {
+            System.out.println(n1 / n2);
+        }
+        else {
+            System.out.println("Invalid response");}
+        System.out.println("Thank you for your submission");
+
+
+
+
+
+
+    }
 }
